797aaa3c6bbf20250d76d930237125142471790e
Jorge García Rivera <jgarciar122@iesagora.caceres>
2024-11-28 20:50:24 +0100

    Primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,112 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
 export class Tab1Page {
+  static series: { title: string; image: string; description: string; vista: boolean }[] = [];
+  segment: string = 'all';
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute, 
+    private alertController: AlertController,
+    private router: Router
+  ) {}
 
+  ionViewWillEnter() {
+    const newSerie = this.route.snapshot.queryParams;
+    if (newSerie['title'] && newSerie['image'] && newSerie['description']) {
+      this.series.push({
+        title: newSerie['title'],
+        image: newSerie['image'],
+        description: newSerie['description'],
+        vista: false  // Añadir vista como false por defecto
+      });
+    }
+  }
+
+  get series() {
+    return Tab1Page.series;
+  }
+
+  filtrarSeries() {
+    if (this.segment === 'vistas') {
+      return this.series.filter(serie => serie.vista);
+    } else if (this.segment === 'no-vistas') {
+      return this.series.filter(serie => !serie.vista);
+    } else {
+      return this.series;
+    }
+  }
+
+  async eliminarSerie(index: number) {
+    const alert = await this.alertController.create({
+      header: 'Confirmar eliminación',
+      message: '¿Estás seguro de que deseas eliminar esta serie?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Eliminar',
+          handler: () => {
+            this.series.splice(index, 1);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async editarSerie(index: number) {
+    const serie = this.series[index];
+    const alert = await this.alertController.create({
+      header: 'Editar Serie',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          placeholder: 'Título',
+          value: serie.title,
+        },
+        {
+          name: 'image',
+          type: 'text',
+          placeholder: 'URL de la imagen',
+          value: serie.image,
+        },
+        {
+          name: 'description',
+          type: 'textarea',
+          placeholder: 'Descripción',
+          value: serie.description,
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Guardar',
+          handler: (data) => {
+            this.series[index] = {
+              ...serie,
+              title: data.title,
+              image: data.image,
+              description: data.description,
+            };
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Tab1Page } from '../tab1/tab1.page';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
 })
 export class Tab2Page {
+  nuevaSerie = {
+    title: '',
+    description: '',
+    image: '',
+    vista: false  // Añadimos la propiedad 'vista' aquí también
+  };
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  guardarSerie() {
+    if (this.nuevaSerie.title && this.nuevaSerie.description && this.nuevaSerie.image) {
+      // Añadir la nueva serie a la lista de series en Tab1Page
+      Tab1Page.series.push({ ...this.nuevaSerie });
+
+      // Limpiar los campos después de guardar la serie
+      this.nuevaSerie = { title: '', description: '', image: '', vista: false };
+
+      // Navegar a la página Tab1
+      this.router.navigate(['/tabs/tab1']);
+    } else {
+      alert('Por favor, complete todos los campos.');
+    }
+  }
 }
